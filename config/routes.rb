1982838fce2c43643cd497b2dff8bc41a73093d1
juyoung235 <jjyoung235@gmail.com>
2023-03-29 09:48:09 -0400

    rails html error control

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'menu_pages/home'
+  get 'menu_pages/help'
+  get 'menu_pages/about'
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
