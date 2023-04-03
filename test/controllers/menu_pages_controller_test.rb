b5e2dfbb99ab403c198970fcf3f40c49da0cfef1
juyoung235 <jjyoung235@gmail.com>
2023-04-03 09:45:54 -0400

    23/04/03 update

--- test/controllers/menu_pages_controller_test.rb
+++ test/controllers/menu_pages_controller_test.rb
@@ -4,13 +4,13 @@ class MenuPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get menu_pages_home_url
     assert_response :success
-    assert_select "title"
+    assert_select "title", "Home | Micropost app"
   end
 
   test "should get help" do
     get menu_pages_help_url
     assert_response :success
-    assert_select "title"
+    assert_select "title", "Home | Micropost app"
   end
 
   test "should get about" do
