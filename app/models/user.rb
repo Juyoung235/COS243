df3727bf4b59537b5339e93e917db20599a0baf8
juyoung235 <jjyoung235@gmail.com>
2023-04-21 09:25:09 -0400

    add some files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
+    Validates :email, format: {with: VALID_EMAIL_REGEX}
+    Validates :name, presence: true, length: {maximum: 50}
+    has_many :Micropost, dependent: :destroy
 end
