4b2e037e22378a68531814427b1aa93e0a4d0a65
juyoung235 <jjyoung235@gmail.com>
2023-04-21 09:44:06 -0400

    0421 fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = ^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
     Validates :email, format: {with: VALID_EMAIL_REGEX}
     Validates :name, presence: true, length: {maximum: 50}
-    has_many :Micropost, dependent: :destroy
+    has_many :microposts, dependent: :destroy
 end
